689fcc5849c4022b093926965d591bee90d6291e
babyoung666 <1515251178@qq.com>
2022-03-24 22:18:19 +0800

    Update CorsConfig.java

--- src/main/java/com/cinle/wowcheat/Config/CorsConfig.java
+++ src/main/java/com/cinle/wowcheat/Config/CorsConfig.java
@@ -16,18 +16,17 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("127.0.0.1")  //springboot2.6 之后强制指定，不给使用*
+                //.allowedOrigins("127.0.0.1")  //不能与一起使用allowedOriginPatterns
                 .allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowedOriginPatterns("*")
                 .allowCredentials(true)
                 .maxAge(3600)
-               /* .exposedHeaders("Access-Control-Allow-Headers",
+                .exposedHeaders("Access-Control-Allow-Headers",
                 "Access-Control-Allow-Methods",
                 "Access-Control-Allow-Origin",
                 "Access-Control-Max-Age",
-                "X-Frame-Options")*/
-
+                "X-Frame-Options")
                 ;
     }
 }
