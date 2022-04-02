9ff90b07aeb8fd264212dc0c473f230c9a58f972
babyoung666 <1515251178@qq.com>
2022-04-02 22:04:01 +0800

    Update SendMailServices.java

--- src/main/java/com/cinle/wowcheat/Service/SendMailServices.java
+++ src/main/java/com/cinle/wowcheat/Service/SendMailServices.java
@@ -5,10 +5,19 @@ import com.cinle.wowcheat.Constants.MyConst;
 import com.cinle.wowcheat.Enum.MailTypeEnum;
 import com.cinle.wowcheat.Event.SendMailEvent;
 import com.cinle.wowcheat.Vo.MailMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationEvent;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 
 /**
  * @Author JunLe
@@ -16,6 +25,18 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class SendMailServices {
+
+    private final Logger logger = LoggerFactory.getLogger(SendMailServices.class);
+
+
+    /**
+     * 获取发送账户
+     */
+    @Value("${spring.mail.username}")
+    private String userName;
+
+    @Autowired
+    private JavaMailSender mailSender;
     @Autowired
     private ApplicationContext applicationContext;
 
@@ -23,6 +44,13 @@ public class SendMailServices {
     @Autowired
     private VerifyService verifyService;
 
+    /**
+     * 事件通知形式发送邮件
+     * @param to
+     * @param subject
+     * @param content
+     * @param type
+     */
     public void sendMail(String to, String subject, String content, MailTypeEnum type){
         MailMessage mailMessage = new MailMessage();
         mailMessage.setTo(to).setSubject(subject).setContent(content).setType(type);
@@ -51,6 +79,51 @@ public class SendMailServices {
         long wait = MyConst.EMAIL_CODE_WAIT_TIME - (MyConst.CODE_KEEPALIVE_TIME - time);
         return  wait;
 
+    }
+
+
+    /**
+     * 发送简单邮件
+     * 使用springEvent异步发送
+     */
+
+    public void sendSimpleMail(MailMessage mailMessage) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(userName);
+        message.setTo(mailMessage.getTo());
+        message.setSubject(mailMessage.getSubject());
+        message.setText(mailMessage.getContent());
+        try {
+            mailSender.send(message);
+            logger.info("成功发送邮件给{}",  mailMessage.getTo());
+        } catch (Exception e) {
+            logger.info("发送邮件给{}失败，原因:{}", mailMessage.getTo(), e.getMessage());
+        }
+
+    }
+
+    /**
+     * 发送HTML邮件
+     */
+
+    public void sendHtmlMail(MailMessage mailMessage) {
+        //使用MimeMessage，MIME协议
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper; //MimeMessageHelper帮助我们设置更丰富的内容
+        try {
+            helper = new MimeMessageHelper(message, true);
+            helper.setFrom(userName);
+            helper.setTo(mailMessage.getTo());
+            helper.setSubject(mailMessage.getSubject());
+            helper.setText(mailMessage.getContent(), true); //true开启支持HTML
+            mailSender.send(message);
+
+            logger.info("成功发送html邮件给{}", mailMessage.getTo());
+        } catch (MessagingException e) {
+            logger.error("发送html邮件给{}失败，原因:{}",  mailMessage.getTo(),e.getMessage());
+            // e.printStackTrace();
+
+        }
     }
 
 }
