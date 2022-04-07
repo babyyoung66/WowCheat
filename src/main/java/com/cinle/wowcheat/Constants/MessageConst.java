b01fb2cfd88683968546db0adb5140e83e983f2e
babyoung666 <1515251178@qq.com>
2022-04-07 22:29:44 +0800

    Update MessageConst.java

--- src/main/java/com/cinle/wowcheat/Constants/MessageConst.java
+++ src/main/java/com/cinle/wowcheat/Constants/MessageConst.java
@@ -1,5 +1,8 @@
 package com.cinle.wowcheat.Constants;
 
+import java.util.Calendar;
+import java.util.Date;
+
 /**
  * @Author JunLe
  * @Time 2022/3/5 1:44
@@ -16,5 +19,15 @@ public class MessageConst {
      * 分页数量
      */
     public static final int Page_Num = 40;
+
+    /**
+     * 计算查询开始时间
+     * @return
+     */
+    public static Date getQueryStartTime(){
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DATE, - MessageConst.Message_Expiration);
+        return calendar.getTime();
+    }
 
 }
