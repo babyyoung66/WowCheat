9b564e639fc975e286b1e3d30289e8563feebe8b
babyoung666 <1515251178@qq.com>
2022-04-07 22:29:34 +0800

    Update SwaggerConfig.java

--- src/main/java/com/cinle/wowcheat/Config/SwaggerConfig.java
+++ src/main/java/com/cinle/wowcheat/Config/SwaggerConfig.java
@@ -1,25 +1,23 @@
 package com.cinle.wowcheat.Config;
 
-import io.swagger.annotations.Api;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.oas.annotations.EnableOpenApi;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @Author JunLe
  * @Time 2022/2/22 14:58
  * 默认访问地址 ：swagger-ui.html 或 swagger-ui/index.html
  * 配置文件 swagger.enable 值为true时启用
+ * model实体类只有在controller中有引用才会生效
  */
 @ConditionalOnProperty(name = "swagger.enable",havingValue = "true")
 @Configuration
@@ -50,26 +48,25 @@ public class SwaggerConfig {
                 .apiInfo(apiInfo()).groupName("Controller")
                 .select()
                 //为controller包路径
-                //.apis(RequestHandlerSelectors.basePackage("com.xxx.xx.Controller"))
+                .apis(RequestHandlerSelectors.basePackage("com.cinle.wowcheat.Controller"))
                 //生成使用了Api注解的类的文档
-                .apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
+                //.apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
                 //包下的所有路径，不做分组
-                .paths(PathSelectors.any())
+                //.paths(PathSelectors.any())
                 .build();
         return docket;
     }
 
     //分组
-   /* @Bean
-    public Docket createRestApiForAddress(){
-        Docket docket = new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(apiInfo()).groupName("Role接口组")
-                .select()
-                //为controller包路径
-                .apis(RequestHandlerSelectors.basePackage("com.cinle.wowcheat.Controller.RoleController"))
-                .build();
-        return docket;
-    }
-*/
+//    @Bean
+//    public Docket createRestApiForAddress(){
+//        Docket docket = new Docket(DocumentationType.SWAGGER_2)
+//                .apiInfo(apiInfo()).groupName("Model")
+//                .select()
+//                .apis(RequestHandlerSelectors.basePackage("com.cinle.wowcheat.Model"))
+//                .paths(PathSelectors.any())
+//                .build();
+//        return docket;
+//    }
 
 }
