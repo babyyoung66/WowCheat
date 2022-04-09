633280a859c50a0ce03bb6f30aa8e18ed4863809
babyoung666 <1515251178@qq.com>
2022-04-10 00:17:46 +0800

    Update WowCheatApplication.java

--- src/main/java/com/cinle/wowcheat/WowCheatApplication.java
+++ src/main/java/com/cinle/wowcheat/WowCheatApplication.java
@@ -1,16 +1,25 @@
 package com.cinle.wowcheat;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+@MapperScan("com.*.*.Dao")
 @EnableAsync//启用异步处理
 @SpringBootApplication
-public class WowCheatApplication {
+public class WowCheatApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(WowCheatApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(WowCheatApplication.class);
+    }
 }
+
