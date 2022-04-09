f6c7b389b3af2ab9654011c951cfb7d3b73e88e3
babyoung666 <1515251178@qq.com>
2022-04-10 00:15:53 +0800

    Create CustomerException.java

---
+++ src/main/java/com/cinle/wowcheat/Exception/CustomerException.java
@@ -0,0 +1,11 @@
+package com.cinle.wowcheat.Exception;
+
+/**
+ * @Author JunLe
+ * @Time 2022/4/9 2:44
+ */
+public class CustomerException extends Exception {
+    public CustomerException(String message) {
+        super(message);
+    }
+}
