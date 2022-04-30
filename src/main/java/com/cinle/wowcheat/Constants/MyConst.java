64bb550cd05f452d16e99a2fa5dc6265e4c13cf9
babyoung666 <1515251178@qq.com>
2022-04-30 16:33:52 +0800

    Update MyConst.java

--- src/main/java/com/cinle/wowcheat/Constants/MyConst.java
+++ src/main/java/com/cinle/wowcheat/Constants/MyConst.java
@@ -35,7 +35,22 @@ public class MyConst{
     public static final String TOKEN_PRIVATE_KEY = "eyJzdWIiOiIxMjMiLCJpc3MiOiJXT1dfQ0hFQVQiLCJleHAiOjE2NDU4MTcxNTAsImlhdCI6MTY0NTc3Mzk1MH0";
 
 
+    /**
+     * token签名
+     */
     public static final String TOKEN_ISSUER = "WOW_CHEAT";
 
+
+    /**
+     * 默认群聊id
+     * 用户注册后默认添加
+     */
+    public static final String DEFAULT_GROUP_ID = "sysTestGroup001";
+
+    /**
+     * 默认管理员id
+     * 用户注册后默认添加
+     */
+    public static final String DEFAULT_ADMIN_ID = "62124fee77b646417ced30b9";
 
 }
