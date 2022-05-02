547bb9520f846aee3a981123ba1ac802a4cf62a0
babyoung666 <1515251178@qq.com>
2022-05-03 01:52:34 +0800

    Update DesensitizedUtil.java

--- src/main/java/com/cinle/wowcheat/Utils/DesensitizedUtil.java
+++ src/main/java/com/cinle/wowcheat/Utils/DesensitizedUtil.java
@@ -7,20 +7,20 @@ package com.cinle.wowcheat.Utils;
 public class DesensitizedUtil {
 
     public static String mobilePhone(String str){
-        if (str=="" || str == null){
-            return null;
+        if (str == null || str.trim()=="" ||  str.length() < 6){
+            return str;
         }
         String target = str.substring(2,str.length() -4);
         return str.replace(target,"*****");
     }
 
     public static String email(String str){
-        if (str.trim()=="" || str == null){
-            return null;
+        if (str == null || str.trim()=="" || str.length() == 0){
+            return str;
         }
         int index = str.indexOf("@");
         if (index < 0){
-            return null;
+            return str;
         }
         if (index < 5){
            return  str.replace(str.substring(1,index),"******");
