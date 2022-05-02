3616388d8486d274044ed2db00bb7e2f30e3281e
babyoung666 <1515251178@qq.com>
2022-05-03 01:50:39 +0800

    Update GroupMemberDao.java

--- src/main/java/com/cinle/wowcheat/Dao/GroupMemberDao.java
+++ src/main/java/com/cinle/wowcheat/Dao/GroupMemberDao.java
@@ -13,37 +13,35 @@ import java.util.List;
 public interface GroupMemberDao {
     int deleteByPrimaryKey(Integer autoId);
 
-    int insert(GroupMember record);
 
     int insertSelective(GroupMember record);
 
     GroupMember selectByPrimaryKey(Integer autoId);
 
-    GroupMember selectByGroupIdAndMemberId(@Param("groupId")String groupId,@Param("memberId")String memberId);
+    GroupMember selectByGroupIdAndMemberId(@Param("groupId") String groupId, @Param("memberId") String memberId);
 
     int updateByPrimaryKeySelective(GroupMember record);
 
-    int updateByPrimaryKey(GroupMember record);
 
-    List<String> getMemberIdListByGroupId(@Param("groupId")String groupId);
+    List<String> getMemberIdListByGroupId(@Param("groupId") String groupId);
 
-    List<String> getAdminIdListByGroupId(@Param("groupId")String groupId);
+    List<String> getAdminIdListByGroupId(@Param("groupId") String groupId);
 
-    List<GroupMember> getGroupMembers(@Param("groupId")String groupId);
+    List<GroupMember> getGroupMembers(@Param("groupId") String groupId);
 
     /**
      * @param groupId
      * @return 只查询 user_uuid,member_role, member_status 三个字段
      */
-    List<GroupMember> getGroupMembersForSendMessage(@Param("groupId")String groupId);
+    List<GroupMember> getGroupMembersForSendMessage(@Param("groupId") String groupId);
 
-    int getMemberTotalByGroupId(@Param("groupId")String groupId);
+    int getMemberTotalByGroupId(@Param("groupId") String groupId);
 
-    int getAdminTotalByGroupId(@Param("groupId")String groupId);
+    int getAdminTotalByGroupId(@Param("groupId") String groupId);
 
-    int updateLastCheatTime(@Param("userId")String userId,@Param("groupId")String groupId,@Param("time") Date time);
+    int updateLastCheatTime(@Param("userId") String userId, @Param("groupId") String groupId, @Param("time") Date time);
 
-    int exitGroup(@Param("userId")String userId,@Param("groupId")String groupId);
+    int exitGroup(@Param("userId") String userId, @Param("groupId") String groupId);
 
     /**
      * 获取多个群组所有的member
@@ -54,4 +52,10 @@ public interface GroupMemberDao {
      * 获取用户所有群组的uuid
      */
     List<String> selectGroupIdListByUserUuid(@Param("userUuid") String userUuid);
+
+    int updateMemberStatus(@Param("userId") String userId, @Param("groupId") String groupId, @Param("status") int status);
+
+    int updateNotifyStatus(@Param("userId") String userId, @Param("groupId") String groupId, @Param("status") int status);
+
+    int updateByUerIdAndGroupIdSelective(GroupMember record);
 }
